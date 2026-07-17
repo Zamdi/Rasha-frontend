f3f68c810900515e3e57dbdeaef232d85c98b68c
Omer <85848457+Zamdi@users.noreply.github.com>
2026-07-17 13:25:56 +0300

    Rasha Customer Portal

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AppProvider } from './context/AppContext'
+import Navbar from './components/Navbar'
+import MobileNav from './components/MobileNav'
+import Toast from './components/Toast'
+import ScrollToTop from './components/ScrollToTop'
+import Home from './pages/Home'
+import Booking from './pages/Booking'
+import Confirmation from './pages/Confirmation'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Loyalty from './pages/Loyalty'
+import Contact from './pages/Contact'
+import Privacy from './pages/Privacy'
+import Terms from './pages/Terms'
+
+export default function App() {
+  return (
+    <AppProvider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Navbar />
+        <Toast />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book" element={<Booking />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/loyalty" element={<Loyalty />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
+        <MobileNav />
+      </BrowserRouter>
+    </AppProvider>
+  )
+}
